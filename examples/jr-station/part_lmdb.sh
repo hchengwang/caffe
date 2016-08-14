ac7311f1405c1e426eaef3c09fba3cf35efa98a9
chunchih <jerry042827@gmail.com>
2016-08-15 03:27:00 +0800

    add part_Dict.prototxt

---
+++ examples/jr-station/part_lmdb.sh
@@ -0,0 +1,9 @@
+MY=examples/jr-station
+
+echo "Create part lmdb.."
+rm -rf $MY/img_part_lmdb
+build/tools/convert_imageset  --resize_height=32 --resize_width=100 ~/caffe/data/vgg_data/data_set/ $MY/part.txt $MY/img_part_lmdb
+
+echo "All Done.."
+
+
