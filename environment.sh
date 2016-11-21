50667e69009744a8da96acfaa4c37580ac6f3853
hchengwang <hchengwang@gmail.com>
2016-11-21 12:19:59 +0800

    add font and perspective state in text-renderer

---
+++ environment.sh
@@ -0,0 +1,5 @@
+# added by Anaconda2 4.1.1 installer
+export PATH=/home/joinet/anaconda2/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/lib64:/home/joinet/anaconda2/lib
+
+export PYTHONPATH=/home/joinet/caffe/python:$PYTHONPATH
